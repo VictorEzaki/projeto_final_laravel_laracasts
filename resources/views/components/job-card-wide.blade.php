39de56a4cc25e96026480505724ed1158f2bfbc8
Victor Ezaki <victorezaki19@gmail.com>
2025-12-30 19:35:11 -0300

    estrutura de estilização finalizada

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,19 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+
+        <h3 class="font-bold text-xg mt-3">Video Producer</h3>
+
+        <p class="text-sm text-gray-400 mt-auto">Full time - From $60,000</p>
+    </div>
+
+    <div class="space-x-1">
+        <x-job-tag>Tag</x-job-tag>
+        <x-job-tag>Tag</x-job-tag>
+        <x-job-tag>Tag</x-job-tag>
+    </div>
+</x-panel>
